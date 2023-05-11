425a2e6472009bbe3497999647d407cca1d1eb93
Tim <tim@vyg.co.nz>
2023-05-11 16:56:17 +1200

    fix reverse button and shiz

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -8,7 +8,7 @@ const Metrics = ({
   distance: number;
 }) => {
   return (
-    <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 flex justify-center items-center bg-gray-800 rounded-lg h-8 z-50 font-orbitron w-40">
+    <div className="fixed bottom-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center bg-gray-800 rounded-lg h-8 z-50 font-orbitron w-40">
       <p className="text-white text-sm md:text-base altitude">{`${altitude}m`}</p>
       <p
         className="text-white text-base md:text-lg mx-2"
