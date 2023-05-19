cc44ce68c527582ce383f7e0f909cd375252ee6d
Tim <tim@vyg.co.nz>
2023-05-19 17:41:14 +1200

    idk

--- app/api/getWalks/route.ts
+++ app/api/getWalks/route.ts
@@ -3,13 +3,19 @@ import path from "path";
 import fs from "fs";
 
 export async function GET() {
-  const hikes: string[] = [];
+  const listOfHikes: string[] = [];
   const walkDirectory = path.join(process.cwd(), "config");
   const files = fs.readdirSync(walkDirectory);
 
   files.forEach(function (file) {
-    hikes.push(file);
+    listOfHikes.push(file);
   });
+
+  const hikes = [];
+  for (const hike of listOfHikes) {
+    const hikeMeta = await import(`config/${hike}/meta.json`);
+    hikes.push({ name: hike, info: hikeMeta });
+  }
 
   return NextResponse.json({ hikes });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -29,33 +29,40 @@ export default function Home() {
     fetchWalks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   return (
     <>
       <title>NZ Hikes</title>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="text-2xl font-bold dark pb-5">Hikes:</h2>
         {loading ? (
           <Loader />
         ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3">
-            {hikes.map((hike: string) => (
-              <Link key={hike} href={`/${hike}`}>
-                <div className="relative border-2 border-gray-300 rounded-md p-4 hover:bg-gray-100 transition-colors duration-200 ease-in-out">
-                  <h2 className="text-sm text-center font-medium">
-                    {formatText(hike)}
-                  </h2>
-                </div>
-              </Link>
-            ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
+            {hikes.map((hike: { name: string; info: any }) => {
+              const isGreatWalk = hike.info.tags.includes("Great Walk");
+              return (
+                <Link key={hike.name} href={`/${hike.name}`}>
+                  <div className="stats shadow w-full sm:max-w-sm">
+                    <div className="stat p-5">
+                      <div className={`stat-title font-normal`}>
+                        {hike.info.tags.join(" • ")}
+                      </div>
+                      <div
+                        className={`stat-value ${
+                          isGreatWalk ? "text-primary" : "text-secondary"
+                        }`}
+                      >
+                        {formatText(hike.name)}
+                      </div>
+                      <div className="stat-desc text font-black">
+                        {hike.info.info.chipMessages.slice(0, 3).join(" • ")}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         )}
-
-        <footer className="fixed bottom-0 w-full text-center text-gray-600">
-          <div className="py-2 text-xs">
-            <p>* For scroggin munchers only</p>
-          </div>
-        </footer>
       </div>
     </>
   );
